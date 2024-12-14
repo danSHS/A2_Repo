f6cba1ab899d0efe188bedc5a603257208e2e919
danSHS <142699258+danSHS@users.noreply.github.com>
2024-12-13 22:04:15 -0500

    woah

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
- res.send('Hello from Lets be men again');
+ res.send('Hello from Lets be real men again type');
 });
 app.listen(PORT, () => {
  console.log(`App running on http://localhost:${PORT}`);
