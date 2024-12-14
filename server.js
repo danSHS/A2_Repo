bce7beae3d7b550361ce901994309eed59bfdc7c
Alfred Adenekan <142699258+danSHS@users.noreply.github.com>
2024-12-13 22:10:28 -0500

    Update server.js for demo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
- res.send('Today is friday');
+ res.send('Today is friday, tomorrow will be saturday');
 });
 app.listen(PORT, () => {
  console.log(`App running on http://localhost:${PORT}`);
